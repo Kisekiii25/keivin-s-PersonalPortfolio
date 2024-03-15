0f8e86da5ec2413c80016bc2fc0082bdec2df17d
Keivin <129949532+Kisekiii25@users.noreply.github.com>
2024-03-15 03:20:36 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let navButton = document.getElementsByClassName("yo")[0]
 let navLinks = document.getElementsByClassName("linksContainer")[0]
 
-
 navButton.addEventListener("click", () =>{
     navLinks.classList.toggle("active");
 })
-
